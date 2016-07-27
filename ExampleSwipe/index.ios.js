a9288d8ee984da0433351c2403d5f6f58a9be5ee
m3vaz <m3vaz@users.noreply.github.com>
2016-07-27 13:43:58 -0400

    minimal state

--- ExampleSwipe/index.ios.js
+++ ExampleSwipe/index.ios.js
@@ -11,21 +11,24 @@ import {
   Text,
   View
 } from 'react-native';
+import SwipeSelector from 'react-native-swipe-selector';
 
 class ExampleSwipe extends Component {
+//   // <Text style={styles.welcome}>
+//   //   Welcome to React Native!
+//   // </Text>
+//   // <Text style={styles.instructions}>
+//   //   To get started, edit index.ios.js
+//   // </Text>
+// {/*<Text style={styles.instructions}>*/}
+// {/*Press Cmd+R to reload,{'\n'}*/}
+// {/*Cmd+D or shake for dev menu*/}
+// {/*</Text>*/}
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <SwipeSelector/>
+
       </View>
     );
   }
